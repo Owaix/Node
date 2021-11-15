fdd5643aaa13defdb63b4617c1a2d85493991861
owaix <aowais37@gmail.com>
2021-11-15 11:42:04 +0500

    dsadad

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,61 +1,61 @@
 'use strict';
 
-const Employee = require('../models/employee.model');
+const BannerVehincle = require('../models/BannerVehincle.model');
 
-exports.findAll = function(req, res) {
-  Employee.findAll(function(err, employee) {
-    console.log('controller')
-    if (err)
-    res.send(err);
-    console.log('res', employee);
-    res.send(employee);
-  });
-};
-
-
-exports.create = function(req, res) {
-    const new_employee = new Employee(req.body);
-
-    //handles null error 
-   if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Please provide all required field' });
-    }else{
-        Employee.create(new_employee, function(err, employee) {
-            if (err)
-            res.send(err);
-            res.json({error:false,message:"Employee added successfully!",data:employee});
-        });
-    }
-};
-
-
-exports.findById = function(req, res) {
-    Employee.findById(req.params.id, function(err, employee) {
+exports.findAll = function (req, res) {
+    BannerVehincle.findAll(function (err, BannerVehincle) {
+        console.log('controller')
         if (err)
-        res.send(err);
-        res.json(employee);
+            res.send(err);
+        console.log('res', BannerVehincle);
+        res.send(BannerVehincle);
     });
 };
 
 
-exports.update = function(req, res) {
-    if(req.body.constructor === Object && Object.keys(req.body).length === 0){
-        res.status(400).send({ error:true, message: 'Please provide all required field' });
-    }else{
-        Employee.update(req.params.id, new Employee(req.body), function(err, employee) {
+exports.create = function (req, res) {
+    const new_BannerVehincle = new BannerVehincle(req.body);
+
+    //handles null error 
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.status(400).send({ error: true, message: 'Please provide all required field' });
+    } else {
+        BannerVehincle.create(new_BannerVehincle, function (err, BannerVehincle) {
             if (err)
-            res.send(err);
-            res.json({ error:false, message: 'Employee successfully updated' });
+                res.send(err);
+            res.json({ error: false, message: "BannerVehincle added successfully!", data: BannerVehincle });
         });
     }
-  
 };
 
 
-exports.delete = function(req, res) {
-  Employee.delete( req.params.id, function(err, employee) {
-    if (err)
-    res.send(err);
-    res.json({ error:false, message: 'Employee successfully deleted' });
-  });
+exports.findById = function (req, res) {
+    BannerVehincle.findById(req.params.id, function (err, BannerVehincle) {
+        if (err)
+            res.send(err);
+        res.json(BannerVehincle);
+    });
+};
+
+
+exports.update = function (req, res) {
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.status(400).send({ error: true, message: 'Please provide all required field' });
+    } else {
+        BannerVehincle.update(req.params.id, new BannerVehincle(req.body), function (err, BannerVehincle) {
+            if (err)
+                res.send(err);
+            res.json({ error: false, message: 'BannerVehincle successfully updated' });
+        });
+    }
+
+};
+
+
+exports.delete = function (req, res) {
+    BannerVehincle.delete(req.params.id, function (err, BannerVehincle) {
+        if (err)
+            res.send(err);
+        res.json({ error: false, message: 'BannerVehincle successfully deleted' });
+    });
 };
--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,74 +1,74 @@
 'user strict';
 var dbConn = require('./../../config/db.config');
 
-//Employee object create
-var Employee = function(employee){
-    this.first_name     = employee.first_name;
-    this.last_name      = employee.last_name;
-    this.email          = employee.email;
-    this.phone          = employee.phone;
-    this.organization   = employee.organization;
-    this.designation    = employee.designation;
-    this.salary         = employee.salary;
-    this.status         = employee.status ? employee.status : 1;
-    this.created_at     = new Date();
-    this.updated_at     = new Date();
+//ID
+//BannerTitle
+//BanneImg
+//LogoImg	
+
+var BannerVehincle = function (BannerVehincle) {
+    this.BannerTitle = BannerVehincle.BannerTitle;
+    this.BanneImg = BannerVehincle.BanneImg;
+    this.LogoImg = BannerVehincle.LogoImg;
+    // this.status = BannerVehincle.status ? BannerVehincle.status : 1;
+    // this.created_at = new Date();
+    // this.updated_at = new Date();
 };
-Employee.create = function (newEmp, result) {    
-    dbConn.query("INSERT INTO employees set ?", newEmp, function (err, res) {
-        if(err) {
+BannerVehincle.create = function (newEmp, result) {
+    dbConn.query("INSERT INTO tbl_BannerVehincle set ?", newEmp, function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
         }
-        else{
+        else {
             console.log(res.insertId);
             result(null, res.insertId);
         }
-    });           
+    });
 };
-Employee.findById = function (id, result) {
-    dbConn.query("Select * from employees where id = ? ", id, function (err, res) {             
-        if(err) {
+BannerVehincle.findById = function (id, result) {
+    dbConn.query("Select * from tbl_BannerVehincle where ID = ? ", id, function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(err, null);
         }
-        else{
+        else {
             result(null, res);
         }
-    });   
+    });
 };
-Employee.findAll = function (result) {
-    dbConn.query("Select * from employees", function (err, res) {
-        if(err) {
+BannerVehincle.findAll = function (result) {
+    dbConn.query("Select * from tbl_BannerVehincle", function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
-        else{
-            console.log('employees : ', res);  
+        else {
+            console.log('tbl_BannerVehincle : ', res);
             result(null, res);
         }
-    });   
+    });
 };
-Employee.update = function(id, employee, result){
-  dbConn.query("UPDATE employees SET first_name=?,last_name=?,email=?,phone=?,organization=?,designation=?,salary=? WHERE id = ?", [employee.first_name,employee.last_name,employee.email,employee.phone,employee.organization,employee.designation,employee.salary, id], function (err, res) {
-        if(err) {
+BannerVehincle.update = function (id, BannerVehincle, result) {
+    dbConn.query("UPDATE tbl_BannerVehincle SET first_name=?,last_name=?,email=?,phone=?,organization=?,designation=?,salary=? WHERE id = ?", [BannerVehincle.first_name, BannerVehincle.last_name, BannerVehincle.email, BannerVehincle.phone, BannerVehincle.organization, BannerVehincle.designation, BannerVehincle.salary, id], function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
-        }else{   
+        } else {
             result(null, res);
         }
-    }); 
+    });
 };
-Employee.delete = function(id, result){
-     dbConn.query("DELETE FROM employees WHERE id = ?", [id], function (err, res) {
-        if(err) {
+BannerVehincle.delete = function (id, result) {
+    dbConn.query("DELETE FROM tbl_BannerVehincle WHERE id = ?", [id], function (err, res) {
+        if (err) {
             console.log("error: ", err);
             result(null, err);
         }
-        else{
+        else {
             result(null, res);
         }
-    }); 
+    });
 };
 
-module.exports= Employee;
+module.exports = BannerVehincle;
